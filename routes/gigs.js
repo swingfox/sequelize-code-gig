5a852df499c66a98660854b7e8847f5f0be7564c
David Ramirez <drthrax.ramirez@gmail.com>
2019-09-21 22:05:16 +0800

    some ui & handlebars

--- routes/gigs.js
+++ routes/gigs.js
@@ -12,8 +12,11 @@ router.get("/", (req, res) =>
     .catch(err => console.log(err))
 );
 
+// Display add gig form
+router.get('/add', (req, res) => res.render('add'));
+
 // Add a gig
-router.get('/add', (req, res) => {
+router.post('/add', (req, res) => {
   const data = {
     title: 'Simple wordpress website',
     technologies: 'wordpress, php, html, css',
