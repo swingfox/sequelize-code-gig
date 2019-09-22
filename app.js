1aaec96fcc3193dff06632f09a60e1244be91ab4
David Ramirez <drthrax.ramirez@gmail.com>
2019-09-22 10:23:38 +0800

    Add gig

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ const app = express();
 app.engine('handlebars', exphbs({defaultLayout: 'main'} ));
 app.set('view engine', 'handlebars');
 
+// Body Parser
+app.use(bodyParser.urlencoded({extended : false}));
+
 // Set static folder
 app.use(express.static(path.join(__dirname,'public')));
 
--- routes/gigs.js
+++ routes/gigs.js
@@ -17,27 +17,52 @@ router.get('/add', (req, res) => res.render('add'));
 
 // Add a gig
 router.post('/add', (req, res) => {
-  const data = {
-    title: 'Simple wordpress website',
-    technologies: 'wordpress, php, html, css',
-    budget: '$1000',
-    description:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-    contact_email: 'user2@gmail.com'
-  };
+    let { title, technologies, budget, description, contact_email } = req.body;
+    let errors = [];
+    
+    // Validate fields
+    if(!title){
+        errors.push({ text: 'Please add a title'});
+    }
+    if(!technologies){
+        errors.push({ text: 'Please add some technologies'});
+    }
+    if(!description){
+        errors.push({ text: 'Please add a description'});
+    }
+    if(!contact_email){
+        errors.push({ text: 'Please add a contact email'});
+    }
 
-    let { title, technologies, budget, description, contact_email } = data;
-
-    // Insert into table
-    Gig.create({
-        title,
-        technologies,
-        description,
-        budget,
-        contact_email
-    })
-        .then(gig => res.redirect('/gigs'))
-        .catch(err => console.log(err));
+    // Check for errors
+    if(errors.length > 0) {
+        res.render('add', {
+            errors,
+            title,
+            technologies,
+            budget,
+            description,
+            contact_email
+        });
+    } else{
+        if(!budget){
+            budget = 'Unknown';
+        } else {
+            budget = `$${budget}`;
+        }
+        // Make lowercase and remove space after comma
+        technologies = technologies.toLowerCase().replace(/, /, ',')
+        // Insert into table
+        Gig.create({
+            title,
+            technologies,
+            description,
+            budget,
+            contact_email
+        })
+            .then(gig => res.redirect('/gigs'))
+            .catch(err => console.log(err));
+    }
 });
 
 module.exports = router;
